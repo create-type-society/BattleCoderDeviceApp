ddeca26145db2f47fc4e74d1d342c2f4d9cc357c
hiroki19990625 <hiroki19990625@gmail.com>
2019-08-29 13:06:48 +0900

    加速度センサーの実装

---
+++ Assets/Scripts/Device/AccelerometerDeviceInput.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using UnityEngine;
+
+namespace Device
+{
+    public class AccelerometerDeviceInput : IDeviceInput
+    {
+        public event EventHandler<SendDeviceDataEventArgs> SendDeviceDataEvent;
+
+        public void Update()
+        {
+            var acceleration = Input.acceleration.normalized;
+
+            var ms = new MemoryStream();
+            var bw = new BinaryWriter(ms);
+            bw.Write(acceleration.x);
+            bw.Write(acceleration.y);
+            bw.Write(acceleration.z);
+
+            OnSendDeviceDataEvent(this, new SendDeviceDataEventArgs(DeviceType.Accelerometer, ms.ToArray()));
+
+            bw.Dispose();
+            ms.Dispose();
+        }
+
+        public void OnSendDeviceDataEvent(object sender, SendDeviceDataEventArgs args)
+        {
+            SendDeviceDataEvent?.Invoke(sender, args);
+        }
+    }
+}
